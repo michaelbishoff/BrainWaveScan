d08c8fec0104ff5c1c8a38b28dd9ac30a54fdfa6
Michael Bishoff <bishoff1@umbc.edu>
2014-11-01 04:49:15 -0400

    Updated the script to terminate the process

--- start_processing_brain_waves.py
+++ start_processing_brain_waves.py
@@ -1,9 +1,23 @@
 import subprocess
+import signal
+import time
 
 def main():
-	subprocess.call(["yes"])
-	subprocess.call(["exit 1"])
+	proc = subprocess.Popen(["python", "test.py"], creationflags=0)
 
+	child_pid = proc.pid
+
+	(output, error) = proc.communicate()
+
+	if error:
+		print "Error:", error
+
+	print "Output:", output
+
+	#sleep(1)
+	print child_pid
+	proc.terminate()
+	print child_pid
 
 
 
